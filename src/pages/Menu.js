3ba21d22c0c9679b197e5acadc24b5efa0556511
mehmetdemircioglu <112698694+mehmetdemircioglu@users.noreply.github.com>
2023-02-15 14:05:21 +0300

    edit

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,11 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import MyCard from "../components/Card";
 import Header from "../components/Header";
 const Menu = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await axios.get("http://localhost:8080/api/menu/list");
+
+      if (response.status === 200) {
+        dispatch({ type: "MENU_LOAD", data: { menuList: response.data } });
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
     <div>
       <Header active="menu" />
       <div class="flex justify-content-center flex-wrap card-container yellow-container">
-        
         <MyCard />
         <MyCard />
         <MyCard />
